c6afaa09672bd1bc9813625cde3d22341c3a5956
Jimmy K <jimmy91kim@gmail.com>
2024-05-14 20:06:22 -0700

    fourth commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,34 +4,37 @@ import csv
 csvpath = r"C:\Users\jimmy\OneDrive\Documents\BootCamp_Challenges\PyBank\Resources\budget_data.csv"
 
 total_profit_losses = 0
-previous_profit_losses = None
 max_increase = 0
 max_decrease = 0
 max_increase_date = ""
 max_decrease_date = ""
-
+previous_profit_losses = None
 
 with open(csvpath, encoding='UTF-8') as csvfile:
     csvreader = csv.reader(csvfile)
-    next(csvreader)
+    #Store header
+    header = next(csvreader)
     dates = []
     changes = []
-
+    
+    # Created a loop for rows in csvreader
     for row in csvreader:
         date = row[0]
         dates.append(date)
         profit_losses = int(row[1])
         total_profit_losses += profit_losses
         
-        
+        # Prompting to find the changed value in profit/loss 
         if previous_profit_losses is not None:
             change = profit_losses - previous_profit_losses
-            changes.append(change)
+            changes.append(change)         
 
+            # Find greatest increase in profit value & date
             if change > max_increase:
                 max_increase = change
                 max_increase_date = date
 
+            # Find greatest decrease in profit value & date
             if change < max_decrease:
                 max_decrease = change
                 max_decrease_date = date
@@ -55,3 +58,25 @@ print("                            ")
 print("Greatest Increase in Profits:", max_increase_date, "($" + str(max_increase) + ")")
 print("                            ")
 print("Greatest Decrease in Profits:", max_decrease_date, "($" + str(max_decrease) + ")")
+
+# Results that will be in text
+output_text = (
+    "Financial Analysis\n"
+    "                            \n"
+    "----------------------------\n"
+    "                            \n"
+    f"Total Months: {total_months}\n"
+    "                            \n"
+    f"Total: ${total_profit_losses}\n"
+    "                            \n"
+    f"Average Change: ${round(average_change, 2)}\n"
+    "                            \n"
+    f"Greatest Increase in Profits: {max_increase_date} (${max_increase})\n"
+    "                            \n"
+    f"Greatest Decrease in Profits: {max_decrease_date} (${max_decrease})\n"
+)
+
+# Write the output to a text file
+output_path = r"C:\Users\jimmy\OneDrive\Documents\BootCamp_Challenges\PyBank\analysis\Pybank_result.txt"
+with open(output_path, 'w') as output_file:
+    output_file.write(output_text)
--- Pypoll/main.py
+++ Pypoll/main.py
@@ -5,12 +5,14 @@ csvpath = r"C:\Users\jimmy\OneDrive\Documents\BootCamp_Challenges\Pypoll\Resourc
 
 with open(csvpath, encoding='UTF-8') as csvfile:
     csvreader = csv.reader(csvfile)
-    next(csvreader) 
+    #Store Header
+    header = next(csvreader) 
     ballots = []
     candidates = []
     max_votes = 0
     winner = ""
 
+    # Created a loop for the rows
     for row in csvreader:
         ballot = row[0]
         ballots.append(ballot)
@@ -27,11 +29,13 @@ with open(csvpath, encoding='UTF-8') as csvfile:
     print("                            ")
     print("-------------------------")
 
+    # Created a loop for 'candidate' to only cycle through the unique candidates then to figure out the % of votes and total votes
     for candidate in unique_candidates:
         votes_for_candidate = candidates.count(candidate)
         percentage = (votes_for_candidate / total_votes) * 100
         print(f"{candidate}: {percentage:.3f}% ({votes_for_candidate})")
         
+        # Finding which candidate has the most votes
         if votes_for_candidate > max_votes:
             max_votes = votes_for_candidate
             winner = candidate
@@ -43,8 +47,35 @@ with open(csvpath, encoding='UTF-8') as csvfile:
     print("                            ")
     print("-------------------------")
 
+# Results that will be in text
+output_text = (
+    "Election results\n"
+    "-------------------------\n"
+    "                            \n"
+    f"Total Votes: {total_votes}\n"
+    "                            \n"
+    "-------------------------\n"
+)
 
+# Added the if statement again to have the correct value on the text file
+for candidate in unique_candidates:
+    votes_for_candidate = candidates.count(candidate)
+    percentage = (votes_for_candidate / total_votes) * 100
+    output_text += f"{candidate}: {percentage:.3f}% ({votes_for_candidate})\n"
 
+output_text += (
+    
+    "-------------------------\n"
+    "                            \n"
+    f"Winner: {winner}\n"
+    "                            \n"
+    "-------------------------\n"
+)
+
+# Write the output to a text file
+output_path = r"C:\Users\jimmy\OneDrive\Documents\BootCamp_Challenges\Pypoll\analysis\result_Pypoll.txt"
+with open(output_path, 'w') as output_file:
+    output_file.write(output_text)
     
     
 
